b390263bc2b567dca48b950ef8221ec1e59afc25
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-03 19:40:22 +0000

    play.js

--- play.js
+++ play.js
@@ -1,5 +1,7 @@
 const net = require("net");
 const { connect } = require('./client');
+const { setupInput } = require("./input");
+
 
 
 // establishes a connection with the game server
@@ -11,31 +13,9 @@ const { connect } = require('./client');
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume(); 
-    stdin.on("data", (data) => {
-    handleUserInput(data);
-    });
-    
-    return stdin;
-
-  };
-
-  const handleUserInput = function (data) {
-    if (data === '\u0003') {
-      return process.exit();
-    }
-   
-  };
- 
-setupInput();
 
 console.log("Connecting ...");
 
-module.exports = { setupInput };
 
 
 
