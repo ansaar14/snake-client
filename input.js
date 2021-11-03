920fe46ccbafc76618ec85af7325f00f62d9ca71
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-03 22:35:03 +0000

    wsad movements

--- input.js
+++ input.js
@@ -1,23 +1,38 @@
-const setupInput = function () {
+const net = require("net");
+const { connect } = require('./client');
+
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume(); 
-  stdin.on("data", (data) => {
-  handleUserInput(data);
-  });
-  
+  stdin.on("data",  handleUserInput)
   return stdin;
 
-};
+  };
 
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
     return process.exit();
+  }
+
+  if(key === "w" || key === "W"){
+    connection.write("Move: up")
+  }
+  if(key === "a" || key === "A"){
+    connection.write("Move: left")
+  }
+  if(key === "s" || key === "S"){
+    connection.write("Move: down")
+  }
+  if(key === "d" || key === "D"){
+    connection.write("Move: right")
   }
  
 };
 
-setupInput();
 
 module.exports = { setupInput };
