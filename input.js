e74df0a51fd60f142c9e56e728d87133c30ea427
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-03 19:40:35 +0000

    input.js

---
+++ input.js
@@ -0,0 +1,23 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume(); 
+  stdin.on("data", (data) => {
+  handleUserInput(data);
+  });
+  
+  return stdin;
+
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    return process.exit();
+  }
+ 
+};
+
+setupInput();
+
+module.exports = { setupInput };
