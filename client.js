3ce472c58ae85f0db6495644ac4a73e7d9c042a9
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-03 23:22:49 +0000

    required constants.js

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 const net = require("net");
-
+const { host, port } = require("./constants");
 
 const connect = function() {
   const conn = net.createConnection({
-    host: "165.227.47.243",// IP address here,
-    port: 50541,// PORT number here,
+    host: host,
+    port: port
 
   });
-
+conn.setEncoding("utf8")
 
 conn.on("connect", () => {
   console.log("Connected");
